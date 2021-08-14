59211b7678a15359889d293b12c779d89ba3096a
unknown <pk9910765616@gmail.com>
2021-08-14 20:22:35 +0530

    bootstrap navbar added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Home from "./Home"
 import About from './About';
 import Service from './Service';
 import Contact from './Contact';
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Navbar from './Navbar';
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Switch>
-        <Route exact  path="/" component={Home} />
-        <Route exact  path="/about" component={About} />
-        <Route exact  path="/service" component={Service} />
-        <Route exact  path="/contact" component={Contact} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/service" component={Service} />
+        <Route exact path="/contact" component={Contact} />
+        <Redirect to="/" />
       </Switch>
     </>
   )
