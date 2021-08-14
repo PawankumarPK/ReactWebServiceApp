c1e3d572675ca7440edc865790d553267142a19e
unknown <pk9910765616@gmail.com>
2021-08-15 00:34:32 +0530

    home page header text added with animated button

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,9 +3,28 @@ import React from "react";
 
 const Home = () => {
   return (
-      <>
-          <h1>Home Welcome</h1>
-      </>
+    <>
+      <div className="container-fluid nav_bg">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <div className="col-md-6 pt5-lg-0 order-2 order-lg-1">
+              <h1>
+                Grow your business with <strong>Gamein.games</strong>
+              </h1>
+              <h2 className="my-3">
+                We are the team of talented developers making websites
+              </h2>
+              <div className="mt-3">
+                <a href="" className="btn btn-outline-primary">{" "} Get Started{" "}</a>
+
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
