64fc1c64d5eeecd219b46d67652268a786389b35
unknown <pk9910765616@gmail.com>
2021-08-15 13:33:09 +0530

    add model for cardviews and refrect code

---
+++ src/SData.jsx
@@ -0,0 +1,31 @@
+import web from "../src/images/freefire2.jpg"
+
+
+const Sdata = [
+    {
+        imgsrc:web,
+        title:"Web Development"
+    },
+    {
+        imgsrc:web,
+        title:"App Development"
+    },
+    {
+        imgsrc:web,
+        title:"Software Development"
+    },
+    {
+        imgsrc:web,
+        title:"Digital Marketing"
+    },
+    {
+        imgsrc:web,
+        title:"Android Development"
+    },
+    {
+        imgsrc:web,
+        title:"Marketing Development"
+    },
+
+]
+export default Sdata;
